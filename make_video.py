028c45df63402a939cb919953d9f1b68e46f6e1e
Erick Viera <erick.viera@appworki.com>
2021-11-26 13:25:26 -0300

    arreglado fps

--- make_video.py
+++ make_video.py
@@ -2,7 +2,7 @@ from bibliotecas import *
 
 def make_video(width, height, img_array, nombre_archivo):
     size = (width,height)
-    out = cv2.VideoWriter('Resultado_' + nombre_archivo ,cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+    out = cv2.VideoWriter('Resultado_' + nombre_archivo ,cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
     
     for i in range(len(img_array)):
         out.write(img_array[i])
