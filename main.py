f5aa4d93506b61d336e49e5d11a35f3bf5744c8a
Daniel <daniel.espinozaf@sansano.usm.cl>
2021-12-09 12:47:46 -0300

    Pisadas en ROI V1.0

--- main.py
+++ main.py
@@ -5,24 +5,48 @@ from localizador_pie import localizador
 from detector_pisadas import detector_pisadas
 from roi import gen_roi, pisadas_roi
 
+'''
+----- DETECTOR DE EVENTOS PARA GENERAR MANUALMENTE EL ROI -----
+'''
+
+ROI_B, count = [], 0
+
+def mouse_callback(event, x, y, flags, params):
+    if event == 2:
+        global ROI_B, count
+        count += 1
+        if (count % 4 == 0):
+            ROI_B.append([x, y])
+            f = open('ROI_B.txt', 'a').write(str(ROI_B) + '\n')
+            ROI_B = []
+            f.close()
+        else:
+            ROI_B.append([x, y])
+        print(ROI_B, count)
 
 def main():
 
     archivo = "Prueba_experimental_B.mp4"
-    balizas = "mask.jpg"
-    mask = cv2.imread(balizas)
+    balizas = "mask.jpg" # (2)
+    mask = cv2.imread(balizas) # (2)
     cap = cv2.VideoCapture(archivo)
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_pose = mp.solutions.pose
 
+    '''
+    ----- PARÁMETROS PARA EL MÓDULO DETECTOR DE PISADAS -----
+    '''
     derecho = [0, 0] # posicion actual del pie derecho
     izquierdo = [0, 0] # posicion actual del pie izquierdo
     tol_x = 8 # toleracia para detectar pisada en eje x
     tol_y = 5 # toleracia para detectar pisada en eje y
 
-    src, contours, points = gen_roi(mask.shape[0], mask.shape[1])
-    cv2.imshow("ROI", src)
+    '''
+    ----- GENERAR LAS REGIONES DE INTERÉS (ACTUALMENTE Prueba_experimental_B.mp4) -----
+    '''
+    src, points_and_contours = gen_roi(mask.shape[0], mask.shape[1]) # se usa el frame obtenido en (2) para no iterar en el While
+    cv2.imshow("ROI", src) # mostrar las regiones de interés obtenidas manualmente
 
     with mp_pose.Pose(static_image_mode=
         False) as pose:
@@ -39,13 +63,23 @@ def main():
             
             baricentro_derecho, baricentro_izquierdo = localizador(resultados, width, height, mp_pose)
             
-            # MÓDULO DETECTOR DE PISADAS
+            '''
+            ----- MÓDULO DETECTOR DE PISADAS -----
+            '''
             imagen, pisada_x, pisada_y = detector_pisadas(imagen, [baricentro_derecho, baricentro_izquierdo], [tol_x, tol_y], [derecho, izquierdo])
 
-            # MÓDULO DETECTOR DE PISADAS EN ZONAS DE INTERÉS
-            imagen = pisadas_roi(imagen, pisada_x, pisada_y, contours, baricentro_derecho, baricentro_izquierdo, points)
+            '''
+            ----- MÓDULO DETECTOR DE PISADAS EN ZONAS DE INTERÉS -----
+            '''
+            imagen = pisadas_roi(imagen, pisada_x, pisada_y, baricentro_derecho, baricentro_izquierdo, points_and_contours)
+            
 
             cv2.imshow("Imagen", imagen)
+            '''
+            ----- (1) GENERAR ROI MANUALMENTE (se usó para generar ROI_B.txt) -----
+            '''
+            # cv2.setMouseCallback("Imagen", mouse_callback)
+
             derecho = baricentro_derecho
             izquierdo = baricentro_izquierdo
             # print("---------------------------")
@@ -55,7 +89,9 @@ def main():
 
             img_array.append(imagen)
 
-            # Obtener un frame de interés para la detección de balizas
+            '''
+            ----- (2) OBTENER UN FRAME DE INTERÉS PARA LA DETECCIÓN DE BALIZAS (ya no se usa, ahora se prueba con 1) -----
+            '''
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv, (160, 50, 50), (180, 255, 255))
             # cv2.imshow("hsv", hsv)
@@ -68,8 +104,6 @@ def main():
             elif k & 0xFF == 32:
                 print("Exportando frame de interés")
                 cv2.imwrite("mask.jpg", mask)
-
-
 
         make_video(width,height, img_array,archivo )
         cap.release()
--- roi.py
+++ roi.py
@@ -1,21 +1,34 @@
 from bibliotecas import *
-
+import ast
 def gen_roi(h, w, mode = 1):
     '''
         mode = 0, ROI para Prueba Experimental A
         mode = 1, ROI para Prueba Experimental B
+        (aún no se implementan las distinciones)
     '''
     src = np.zeros((h, w), dtype = np.uint8)
-    points = np.array([[350, 165], [265, 165], [260, 205], [355, 205]], np.int32).reshape((-1, 1, 2))
-    src = cv2.polylines(src, [points], True, ( 255 ), 2)
-    contours, _ = cv2.findContours(src, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return src, contours, points
+    lines = open('ROI_B.txt', 'r').readlines()
+    ROI = [ast.literal_eval(line[:-1]) for line in lines]
+    points_and_contours = []
+    for roi in ROI:
+        temp = np.zeros((h, w), dtype = np.uint8)
+        p = np.array(roi, np.int32).reshape((-1, 1, 2))
+        temp = cv2.polylines(temp, [p], True, ( 255 ), 2)
+        cont, _ = cv2.findContours(temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        src = cv2.bitwise_or(src, temp)
 
-def pisadas_roi(imagen, pisada_x, pisada_y, contours, baricentro_derecho, baricentro_izquierdo, points):
+        points_and_contours.append((p, cont[0]))
+    return src, points_and_contours
+
+def pisadas_roi(imagen, pisada_x, pisada_y, baricentro_derecho, baricentro_izquierdo, points_and_contours):
+    '''
+        Si ocurrió una pisada, se busca dónde se pisó y se pinta.
+    '''
     if pisada_x or pisada_y:
-        der =  cv2.pointPolygonTest(contours[0], (baricentro_derecho[0], baricentro_derecho[1]), False)
-        izq =  cv2.pointPolygonTest(contours[0], (baricentro_izquierdo[0], baricentro_izquierdo[1]), False)
-        if (der >= 0) or (izq >= 0):
-                imagen = cv2.fillPoly(imagen, [points], (0, 255, 0))
+        for i in range(len(points_and_contours)):
+            der = cv2.pointPolygonTest(points_and_contours[i][1], (baricentro_derecho[0], baricentro_derecho[1]), False)
+            izq = cv2.pointPolygonTest(points_and_contours[i][1], (baricentro_izquierdo[0], baricentro_izquierdo[1]), False)
+            if (der >= 0) or (izq >= 0):
+                    imagen = cv2.fillPoly(imagen, [points_and_contours[i][0]], (0, 255, 0))
     return imagen
     
